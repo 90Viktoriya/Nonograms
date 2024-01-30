ffe0cef28c0a61658941d8d917e14a6eb18be6e9
90Viktoriya <viktoria.kileeva@gmail.com>
2024-01-31 01:01:28 +0300

    feat: add list of pictures for selection

---
+++ nonograms/gameSelection.js
@@ -0,0 +1,63 @@
+import createElement from "./createElement.js";
+import data from "./data.json" assert { type: "json" };
+
+function addSelection(name, key, checked = false) {
+  let div = createElement('div', 'select__item_wrapper');
+  let input = createElement('input', 'select__item', key);
+  input.type = 'radio';
+  input.name = name;
+  input.id = key;
+  input.checked = checked;
+  let label = createElement('label', 'select__label', key);
+  label.htmlFor = key;
+  div.append(input);
+  div.append(label);
+  return div;
+}
+function changeList(e) {
+  let value = '';
+  if (e.target.classList.contains('select__item'))
+    value = e.target.id;
+  else if (e.target.classList.contains('select__label'))
+    value = e.target.htmlFor;
+  if (value) {
+    const picture = e.currentTarget.picture;
+    while (picture.lastChild && picture.lastChild.nodeName === 'DIV') {
+      picture.lastChild.remove();
+    }
+    loadList(value, picture);
+  }
+}
+function loadList(value, picture) {
+  let first = true;
+  data.forEach(element => {
+    if (element.level === value) {
+      picture.append(addSelection('Picture', element.name, first));
+      first = false;
+    }
+  })
+}
+function fillSelect(select_wrapper) {
+  let difficult = createElement('fieldset', 'select__fieldset');
+  difficult.append(createElement('legend', 'select__legend','Game difficulty'));
+  difficult.append(addSelection('difficult', 'easy', true));
+  difficult.append(addSelection('difficult', 'medium'));
+  difficult.append(addSelection('difficult', 'hard'));
+  select_wrapper.append(difficult);
+  let picture = createElement('fieldset', 'select__fieldset');
+  picture.append(createElement('legend', 'select__legend','Game picture'));
+  loadList('easy', picture);
+  select_wrapper.append(picture);
+  difficult.addEventListener('click', changeList);
+  difficult.picture = picture;
+  let btns = createElement('div', 'select__buttons');
+  let btnSelect = createElement('button', 'select__button', 'Select');
+  let btnRandom = createElement('button', 'select__button', 'Random');
+  let btnLoad = createElement('button', 'select__button', 'Load');
+  btns.append(btnSelect);
+  btns.append(btnRandom);
+  btns.append(btnLoad);
+  select_wrapper.append(btns);
+}
+
+export default fillSelect;
--- nonograms/script.js
+++ nonograms/script.js
@@ -3,13 +3,20 @@ import fillClues from "./clues.js";
 import fillData from "./crossData.js"
 import createElement from "./createElement.js";
 import displayTimer from "./timer.js";
+import fillSelect from "./gameSelection.js";
 
 
 let currentGameId = 0;
 let newGame = new game(currentGameId);
 
+let body_wrapper = createElement('div', 'body__wrapper');
+document.body.append(body_wrapper);
+body_wrapper.append(createElement('h1', 'game_title', 'Nonograms'));
 let main_wrapper = createElement('div', 'main__wrapper');
-document.body.append(main_wrapper);
+body_wrapper.append(main_wrapper);
+let select_wrapper = createElement('div', 'select__wrapper');
+fillSelect(select_wrapper);
+main_wrapper.append(select_wrapper);
 let game_wrapper = createElement('div', 'game__wrapper');
 displayTimer(game_wrapper);
 main_wrapper.append(game_wrapper);
